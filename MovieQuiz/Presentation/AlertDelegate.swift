bd7e0d6192fe7be51f7e7be4f59a1c004890749d
NickZhirnoff <nickzhirnoff@gmail.com>
2025-07-13 23:28:16 +0300

    Fix errors

--- MovieQuiz/Presentation/AlertDelegate.swift
+++ MovieQuiz/Presentation/AlertDelegate.swift
@@ -1,10 +1,10 @@
 //
-//  AlertDeligate.swift
+//  AlertDelegate.swift
 //  MovieQuiz
 //
 //  Created by Николай Жирнов on 18.10.2024.
 //
 
-protocol AlertDeligate: AnyObject {
+protocol AlertDelegate: AnyObject {
     func didReceiveResultAlert()
 }
